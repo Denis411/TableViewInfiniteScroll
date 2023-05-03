acd1d55252b64dfb1afaac12b93407c6b492b4fb
Denis <theveryengineer@gmail.com>
2023-05-03 20:34:15 +0500

    Initial Commit

---
+++ TableViewWithActivityIndicator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewWithActivityIndicator
+//
+//  Created by Apex on 3/5/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
